f029225d9ac4db4b4a8d86b6497deda04d83e734
Ruben Vorderman <rubenvorderman@xs4all.nl>
2026-03-12 19:22:10 +0100

    Add C and Python references

---
+++ reference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define FIZZBUZZ_LIMIT (1ULL << 32ULL)
+int main() {
+    for (uint64_t i=1l; i < FIZZBUZZ_LIMIT; i++) {
+        if (i % 15 == 0) {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0) {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0) {
+            printf("Buzz\n");
+        }
+        else {
+            printf("%llu\n", i);
+        }
+    }
+}
---
+++ reference.py
@@ -0,0 +1,17 @@
+"""
+Reference FizzBuzz implementation
+"""
+
+def main():
+    for i in range(1, 2**32):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    main()
