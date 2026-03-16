f1428ae07f5aec4e1ee191bdef9015d39e7ca6f7
Ruben Vorderman <rubenvorderman@xs4all.nl>
2026-03-16 10:21:37 +0100

    Prepare a template beforehand

--- loop_unrolling.c
+++ loop_unrolling.c
@@ -168,16 +168,55 @@ static void initialize_zero_to_999(void) {
 
 static inline size_t number_from_prefix_and_index(
     const char *restrict prefix, size_t prefix_length, size_t index, 
-    char * buffer) {
-        memcpy(buffer, prefix, 20);
+    char * buffer) 
+{
+    /* Using a fixed memcpy size avoids a function call. The largest 
+        integer is 20 digits in size.*/
+    memcpy(buffer, prefix, 20);
         buffer += prefix_length;
         /* Lookup the last 3 digits. */
         size_t pos = index * 4;
         memcpy(buffer, zero_to_999 + pos, 4);
         return prefix_length + 4;
-    }
+}
 
-static size_t fizzbuzz_memoized(uint64_t start, uint64_t stop, char *buffer) {
+static void initialize_unroll_template(
+    char *unroll_template, 
+    char *template_number, 
+    size_t number_of_digits
+) {
+        /* Using a fixed memcpy size avoids a function call. The largest 
+           integer is 20 digits in size.*/
+        char *cursor = unroll_template;
+        memcpy(unroll_template, template_number, 20);  // 1
+        unroll_template[number_of_digits] = '\n';
+        cursor += number_of_digits + 1;
+        memcpy(cursor, template_number, 20);  // 2
+        cursor += number_of_digits + 1;
+        memcpy(cursor, "Fizz\n", 5); // 3
+        cursor += 5;
+        memcpy(cursor, template_number, 20);  // 4
+        cursor += number_of_digits + 1;
+        memcpy(cursor, "Buzz\nFizz\n", 10);  // 5, 6
+        cursor += 10;
+        memcpy(cursor, template_number, 20);  // 7
+        cursor += number_of_digits + 1;
+        memcpy(cursor, template_number, 20);  // 8
+        cursor += number_of_digits + 1;
+        memcpy(cursor, "Fizz\nBuzz\n", 10);  // 9, 10
+        cursor += 10;
+        memcpy(cursor, template_number, 20);  // 11
+        cursor += number_of_digits + 1;
+        memcpy(cursor, "Fizz\n", 5); // 12
+        cursor += 5;
+        memcpy(cursor, template_number, 20);  // 13
+        cursor += number_of_digits + 1;
+        memcpy(cursor, template_number, 20);  // 14
+        cursor += number_of_digits + 1;
+        memcpy(cursor, "FizzBuzz\n", 9);  // 15
+}
+
+static size_t fizzbuzz_memoized(uint64_t start, uint64_t stop, char *restrict buffer) {
     if (start % 1000 != 0 || stop - start > 1000) {
         fprintf(
             stderr, 
@@ -189,9 +228,35 @@ static size_t fizzbuzz_memoized(uint64_t start, uint64_t stop, char *buffer) {
     }
     size_t number_of_decimals = calculate_number_of_decimals(start);
     size_t buffer_size = 0;
-    char prefix[21];
-    write_number(start, number_of_decimals, prefix);
+    char template_number[21];
+    write_number(start, number_of_decimals, template_number);
+    char *prefix = template_number;
     size_t prefix_length = number_of_decimals - 3;
+    
+    /* Template the fizzes and buzzes and numbers. In the unroll loop we 
+       will only replace the last digits. 
+       The largest template will need will have 8 numbers with size of 20,
+       5 fizzes, 3 buzzes (including fizzbuzz) and 15 newlines. This equals
+       8*20 + 5*4 + 3*4 + 15 = 160 + 20 + 12 + 15 = 207. We make the template
+       208 units because that is equal to 13 * 16 and the compiler can use
+       16 byte vectors for copying.
+       Also calculate the offset where the template numbers should be. This
+       is constant for each loop.
+       */
+    char unroll_template[208];
+    initialize_unroll_template(unroll_template, template_number, number_of_decimals);
+    size_t unroll_template_size = 8 * number_of_decimals + 8 * 4 + 15;
+    /* For each offset, number of preceding numbers times length, number
+       off fizzes/buzzes times 4 and number of newline characters + 
+       prefix length. */
+    size_t offset1  = 0 * number_of_decimals + 0 * 4 +  0 + prefix_length; 
+    size_t offset2  = 1 * number_of_decimals + 0 * 4 +  1 + prefix_length;
+    size_t offset4  = 2 * number_of_decimals + 1 * 4 +  3 + prefix_length;
+    size_t offset7  = 3 * number_of_decimals + 3 * 4 +  6 + prefix_length;
+    size_t offset8  = 4 * number_of_decimals + 3 * 4 +  7 + prefix_length;
+    size_t offset11 = 5 * number_of_decimals + 5 * 4 + 10 + prefix_length;
+    size_t offset13 = 6 * number_of_decimals + 6 * 4 + 12 + prefix_length;
+    size_t offset14 = 7 * number_of_decimals + 6 * 4 + 13 + prefix_length;
     size_t index = 0;
     size_t i = start;
     while (i < stop && i % 15 != 1) {
@@ -214,56 +279,27 @@ static size_t fizzbuzz_memoized(uint64_t start, uint64_t stop, char *buffer) {
         index += 1;
         i += 1;
     }   
+
+    /*This is the hot loop where most of the work happens */
     uint64_t unroll_stop = stop - 15;
     for (; i<unroll_stop; i+=15 ) {
-        // 1
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        index += 1;
-        // 2
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        // 3
-        memcpy(buffer + buffer_size, "Fizz\n", 5);
-        buffer_size += 5;
-        index += 2;
-        // 4
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        // 5, 6, 
-        memcpy(buffer + buffer_size, "Buzz\nFizz\n", 10);
-        buffer_size += 10;
-        index += 3;
-        // 7,
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        index += 1;
-        // 8
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        // 9, 10
-        memcpy(buffer + buffer_size, "Fizz\nBuzz\n", 10);
-        buffer_size += 10;
-        index += 3;
-        // 11
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        // 12
-        memcpy(buffer + buffer_size, "Fizz\n", 5);
-        buffer_size += 5;
-        index += 2;
-        // 13
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        index += 1;
-        // 14
-        buffer_size += number_from_prefix_and_index(
-            prefix, prefix_length, index, buffer + buffer_size);
-        // 15
-        memcpy(buffer + buffer_size, "FizzBuzz\n", 9);
-        buffer_size += 9;
-        index += 2;
+        char *restrict cursor = buffer + buffer_size;
+        /* Fixed size memcpy faster than function call */
+        memcpy(cursor, unroll_template, sizeof(unroll_template));
+        memcpy(cursor + offset1,  zero_to_999 + ((index +  0) * 4), 4);
+        memcpy(cursor + offset2,  zero_to_999 + ((index +  1) * 4), 4);
+        memcpy(cursor + offset4,  zero_to_999 + ((index +  3) * 4), 4);
+        memcpy(cursor + offset7,  zero_to_999 + ((index +  6) * 4), 4);
+        memcpy(cursor + offset8,  zero_to_999 + ((index +  7) * 4), 4);
+        memcpy(cursor + offset11, zero_to_999 + ((index + 10) * 4), 4);
+        memcpy(cursor + offset13, zero_to_999 + ((index + 12) * 4), 4);
+        memcpy(cursor + offset14, zero_to_999 + ((index + 13) * 4), 4);
+        cursor += 15;
+        index += 15;
+        buffer_size += unroll_template_size;
     }
+    /* End of hot loop. */
+
     for (; i < stop; i++) {
                 if (i % 15 == 0) {
             memcpy(buffer + buffer_size, "FizzBuzz\n", 9);
